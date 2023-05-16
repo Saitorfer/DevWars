24789f80a9abf92de5e44591505b61523054ee07
Saitorfer <39096665+Saitorfer@users.noreply.github.com>
2023-05-16 15:47:55 +0200

    First Commit
    I change my pc

---
+++ src/Main.elm
@@ -0,0 +1,32 @@
+module Main exposing (..)
+import Browser
+
+
+type Msg
+    = MsgUrlChanged Url.Url
+    | MsgUrlRequested Browser.UrlRequest
+    | MsgSelectorPage 
+    | MsgGamePage
+
+type alias Model {
+     url : Url.Url
+    , navigationKey : Browser.Navigation.Key
+    , modelSelectorPage : SelectorPage.Model
+    , modelGame : GamePage.Model
+    }
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = onUrlChange
+        , onUrlRequest = onUrlRequest
+        }
+
+init : () -> Url.Url -> Navigation.Key -> ( model, Cmd msg )
+init _  url navigationKey= 
+    (initModel url navigationKey, Cmd.none)
+
---
+++ src/SelectorPage.elm
@@ -0,0 +1,1 @@
+module SelectorPage exposing (..)
